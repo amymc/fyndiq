3aeab33177e0b95cebed09c558e784ff0278f5a4
amymc <amy.mccarthy2@mail.dcu.ie>
2015-04-14 01:15:53 +0100

    added off canvas cart and overlay menu

--- js/main.js
+++ js/main.js
@@ -1,99 +1,161 @@
-var modal = (function(){
-	var 
-	method = {},
-	$overlay = $('.overlay'),
-	$modal = $('.modal'),
-	$content = $('.content'),
-	$close = $('.close');
+var skateStore = (function(){
 
-	// Center the modal in the viewport
-	method.center = function () {
-		var top, left;
-
-		top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
-		left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
-
-		$modal.css({
-			top:top + $(window).scrollTop(), 
-			left:left + $(window).scrollLeft()
-		});
-	};
-
-	// Open the modal
-	method.open = function (settings) {
-		$content.empty().append(settings.content);
-
-		$modal.addClass('show');
-
-		$modal.css({
-			width: settings.width || 'auto', 
-			height: settings.height || 'auto'
-		});
-
-		method.center();
-		$(window).bind('resize.modal', method.center);
-	};
-
-	// Close the modal
-	method.close = function () {
-		$modal.removeClass('show');
-		$content.empty();
-		$(window).unbind('resize.modal');
-	};
-
-	$close.click(function(e){
-		e.preventDefault();
-		method.close();
-	});
-
-	return method;
-}());
+    var triggerBttn = $('#toggle'),
+        overlay = $('.menu-overlay'),
+        method = {},
+        $overlay = $('.modal-overlay'),
+        $modal = $('.modal'),
+        $content = $('.content'),
+        $close = $('.close'),
+        $siteWrapper = $('.site-wrapper'),
+        productNumber;
 
 
-$(document).ready(function(){
+    function openMenu() {
 
-	$('.item').click(function(e){
-		var productNumber = $(this).attr( 'id' );
+        triggerBttn.click(toggleOverlay);
 
-		$.ajax({
-			url: "./products/product"+productNumber+".html",
-			cache: false,
-			success: function(html){
-				modal.open({content: $(html)});
-				//modal.open({content: $(html).filter('.product.'+ productNumber)});
-			}
-		});
+        function toggleOverlay() {
+            if ($(overlay).hasClass('open')){
+                $(overlay).removeClass('open');
+                var over = $(overlay).addClass('closed');
+                $(triggerBttn).removeClass('open');
+                setTimeout(function() {
+                    over.removeClass('closed');
+                }, 400);
+            }
+            else{
+                $(overlay).addClass('open');
+                $(triggerBttn).addClass('open');
+            }
+        }        
+    }
 
-		e.preventDefault();
-	});
 
-	
-	simpleCart({
-	    checkout: {
-	      type: "PayPal",
-	      email: "you@yours.com"
-	    },
-	    cartStyle : "table",
-	      cartColumns: [
-			    { attr: "name" , label: "Product" } ,
-			    { attr: "price" , label: "Price", view: 'currency' } ,
-			    { view: "decrement" , label: false , text: "-" } ,
-			    { attr: "quantity" , label: "Qty" } ,
-			    { view: "increment" , label: false , text: "+" } ,
-			    { attr: "total" , label: "Total", view: 'currency' } ,
-			    { view: "remove" , text: "Remove" , label: false }
-			]
-		});	
+    function loadProducts() {
 
-	var cartIcon = $('.cart-icon');
-	var cartDetails = $('.cart-details');
-	var storeItems = $('.store-items');
+        $('.item').click(function(){
+            productNumber = $(this).attr( 'id' );
+            $.ajax({
+                url: "./products/product"+productNumber+".html",
+                cache: false,
+                success: function(html){
+                    openModal({content: $(html)});
+                    //modal.open({content: $(html).filter('.product.'+ productNumber)});
+                }
+            });
+        });
+    }
 
-	$(cartIcon).click(function() {
-		$(cartDetails).toggleClass('visible');
-		if ($(window).width() < 800){ 
-			$(storeItems).toggleClass('fixed');
-		}
-	});
 
-});
+    function openModal(settings){
+        $content.empty().append(settings.content);
+        $siteWrapper.addClass('fixed');
+        $modal.addClass('show');
+        $overlay.addClass('show');
+        $close.click(closeModal);
+    }
+
+
+    function closeModal(){
+        $siteWrapper.removeClass('fixed');
+        $modal.removeClass('show');
+        $overlay.removeClass('show');
+        $content.empty();
+    }
+
+
+    function changeQuantity() {
+
+        $(".modal").on("click", ".icon", function() {
+
+            var $button = $(this);
+            var oldValue = $button.parent().find("input").val();
+            var newVal;
+
+            if ($button.text() == "+") {
+                var newVal = parseFloat(oldValue) + 1;
+            } 
+            else {
+                // Don't allow decrementing below one
+                if (oldValue > 1) {
+                    newVal = parseFloat(oldValue) - 1;
+                } 
+                else {
+                    newVal = 1;
+                }
+            }
+
+            $button.parent().find("input").val(newVal);
+        });
+    }
+
+    function openCart() {
+
+        $('.site-container').click(function(event){
+          toggleSidebar();
+        });
+
+        function toggleSidebar(){
+            if ($('.site-container').hasClass('cart-details-open')){
+                $('.site-container').removeClass('cart-details-open');
+                $('.site-container').removeClass('animation-effect');
+                $('.empty-msg').css('display', 'none');
+                $('.simpleCart_items').css('display', 'block');
+                $('.burger').removeClass('burger-hide');
+            }
+            else if ($(event.target).hasClass('cart-icon')){
+                if(simpleCart.items().length == 0){
+                    $('.empty-msg').css('display', 'block');
+                    $('.simpleCart_items').css('display', 'none');
+                    $('#cart-total').css('display', 'none');
+                }
+                else{
+                    $('.empty-msg').css('display', 'none');
+                    $('.simpleCart_items').css('display', 'block');
+                    $('#cart-total').css('display', 'block');
+                }
+                $('.burger').addClass('burger-hide');
+                $('.site-container').addClass('cart-details-open');
+                $('.site-container').addClass('animation-effect');
+            }
+        }
+    }
+
+    simpleCart({
+        checkout:{
+            type: "PayPal",
+            email: "you@yours.com"
+        },
+        cartStyle : "div",
+        cartColumns: [
+            { view: "image" , attr: "thumb", label: false },
+            { attr: "name" , label: false } ,
+            { attr: "quantity" , label: false },
+            { view: function(item, column){return "x"} , label: false },
+            { attr: "price" , label: false, view: 'currency' },
+            { view: "remove" , text: "Remove" , label: false }
+            ]
+    });
+
+
+    function isCartEmpty(){
+        simpleCart.bind( 'update' , function( item ){
+            if (simpleCart.grandTotal() === 0){
+                $('.empty-msg').css('display', 'block');
+                $('.simpleCart_items').css('display', 'none');
+                $('#cart-total').css('display', 'none');
+             } 
+        });
+    }
+
+    return {
+        openMenu: openMenu(),
+        loadProducts: loadProducts(),
+        changeQuantity: changeQuantity(),
+        openCart: openCart(),
+        simpleCart: simpleCart(),
+        isCartEmpty: isCartEmpty()
+    };
+ 
+})();
